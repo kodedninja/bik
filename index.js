edb7a16fa089361cf17c573f9d8ed2202edf8da0
Hunor Karamán <hunix94@gmail.com>
2018-04-23 10:42:05 +0300

    added

--- index.js
+++ index.js
@@ -8,22 +8,40 @@ function bik(t, f) {
 		t = {}
 	}
 	if (typeof f !== 'function') {
-		throw new Error('bik needs at least a function.')
+		throw new Error('bik needs at least a handler function.')
 	}
 
-	var tree = f(t)
+	var tree = null,
+		_added = false
 
 	t.r = t.render = function() {
 		var newtree = f(t)
+		if (!newtree) {
+			throw new Error('the handler function must return a HTML Node')
+		}
 		nanomorph(tree, newtree)
 	}
 
 	t.a = t.append = function (root) {
+		if (!_added) {
+			tree = f(t)
+		} else {
+			throw new Error('cannot use a component multiple times')
+		}
+
 		root.appendChild(tree)
+		_added = true
 	}
 
 	t.prepend = function (root) {
+		if (!_added) {
+			tree = f(t)
+		} else {
+			throw new Error('cannot use a component multiple times')
+		}
+
 		root.insertBefore(tree, root.firstChild)
+		_added = true
 	}
 
 	return t
